aca49975085ed60c96f9ef07b0dd89fb17928647
Divya Chauhan <divyachauhn20061@gmail.com>
2026-02-28 23:17:24 +0530

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Service_connect.Controllers
+{
+    public class AccountController : Controller
+    {
+        // LOGIN
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string email, string password)
+        {
+            if (email == "admin@company.com" && password == "123456")
+            {
+                return RedirectToAction("Dashboard", "Home");
+            }
+
+            ViewBag.Error = "Invalid Email or Password";
+            return View();
+        }
+
+        // REGISTER / SIGNUP
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
